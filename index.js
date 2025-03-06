16e219a06f1eee5da5ebba4a3f775454369dd07c
unknown <akshadashirke3@example.com>
2025-03-06 17:17:46 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
